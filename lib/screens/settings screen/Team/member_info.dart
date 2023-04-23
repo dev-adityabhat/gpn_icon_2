e578205e141061039d8daaa381f6f440ef218d74
dev-adityabhat <111901647+dev-adityabhat@users.noreply.github.com>
2023-04-23 19:29:05 +0530

    upload
    team page remained

---
+++ lib/screens/settings screen/Team/member_info.dart
@@ -0,0 +1,22 @@
+import 'package:myapp/screens/info screen/tabs/executive screen/executive_item.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/screens/settings%20screen/Team/team_item.dart';
+
+var Mem_Info = [
+  const Members(
+    name: 'Shreyas Shirwadkar',
+    imageUrl: 'assets/images/shreyas.png',
+  ),
+  const Members(
+    name: 'Vedant Aware',
+    imageUrl: 'assets/images/vedant.jpeg',
+  ),
+  const Members(
+    name: 'Aditya Bhat',
+    imageUrl: 'assets/images/aditya.png',
+  ),
+  const Members(
+    name: 'Sahil Sinnarkar',
+    imageUrl: 'assets/images/sahil.png',
+  ),
+];
